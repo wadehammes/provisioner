07114ddf7a7f63ca4744f92e102b2348ae125089
Wade Hammes <wade.hammes@gmail.com>
2024-03-18 20:18:52 -0400

    Chore: create Case Study page

--- src/app/case-study/page.tsx
+++ src/app/case-study/page.tsx
@@ -1,18 +1,41 @@
+import { Metadata } from "next";
 import { draftMode } from "next/headers";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 import { fetchCaseStudies } from "src/contentful/getCaseStudies";
+import { fetchPage } from "src/contentful/getPages";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const page = await fetchPage({
+    slug: "case-study",
+    preview: draftMode().isEnabled,
+  });
+
+  if (!page) {
+    return notFound();
+  }
+
+  return {
+    title: `${page.pageTitle} | Provisioner`,
+    robots: page.enableIndexing ? "index, follow" : "noindex, nofollow",
+    description: page.metaDescription,
+  };
+}
 
 async function CaseStudies() {
-  // Fetch case studies using the content preview
-  // if draft mode is enabled:
   const caseStudies = await fetchCaseStudies({
+    preview: draftMode().isEnabled,
+  });
+
+  const caseStudyPage = await fetchPage({
+    slug: "case-study",
     preview: draftMode().isEnabled,
   });
 
   return (
     <main>
       <div>
-        <h1>Case Studies</h1>
+        <h1>{caseStudyPage?.pageTitle}</h1>
         <ul>
           {caseStudies.map((caseStudy) => (
             <li key={caseStudy.slug}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import type { Metadata } from "next";
+import { draftMode } from "next/headers";
+import { notFound } from "next/navigation";
 import { NewsletterForm } from "src/components/NewsletterForm/NewsletterForm.component";
+import { fetchPage } from "src/contentful/getPages";
 import { Provisioner } from "src/icons/Provisioner.icon";
 
-export const metadata: Metadata = {
-  title: "Provisioner",
-  description: "Together we grow.",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const page = await fetchPage({
+    slug: "home",
+    preview: draftMode().isEnabled,
+  });
+
+  if (!page) {
+    return notFound();
+  }
+
+  return {
+    title: `${page.pageTitle} | Provisioner`,
+    robots: page.enableIndexing ? "index, follow" : "noindex, nofollow",
+    description: page.metaDescription,
+  };
+}
 
 const Home = () => {
   return (
