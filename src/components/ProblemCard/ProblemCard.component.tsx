c4d46218ee950d454e9701cc797533f772936258
Wade Hammes <wade.hammes@gmail.com>
2024-05-21 21:57:50 -0400

    Chore: adding some fun

--- src/components/ProblemCard/ProblemCard.component.tsx
+++ src/components/ProblemCard/ProblemCard.component.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import classNames from "classnames";
+import { useRef } from "react";
 import styles from "src/components/ProblemCard/ProblemCard.module.css";
 import { ProblemType } from "src/types/Problems";
 
@@ -8,10 +11,36 @@ interface ProblemCardProps {
 
 export const ProblemCard = (props: ProblemCardProps) => {
   const { problem } = props;
+  const boundingRef = useRef<DOMRect | null>(null);
 
   return (
-    <div>
-      <div className={classNames(styles.problemCard, styles[`${problem.id}`])}>
+    <div className={styles.cardContainer}>
+      <div
+        onMouseEnter={(e) => {
+          boundingRef.current = e.currentTarget.getBoundingClientRect();
+        }}
+        onMouseMove={(e) => {
+          if (!boundingRef.current) {
+            return;
+          }
+
+          const x = e.clientX - boundingRef.current.left;
+          const y = e.clientY - boundingRef.current.top;
+          const xPer = x / boundingRef.current.width;
+          const yPer = y / boundingRef.current.height;
+          const xRotation = (xPer - 0.5) * 20;
+          const yRotation = (0.5 - yPer) * 20;
+
+          e.currentTarget.style.setProperty("--x-rotation", `${xRotation}deg`);
+          e.currentTarget.style.setProperty("--y-rotation", `${yRotation}deg`);
+        }}
+        onMouseLeave={(e) => {
+          boundingRef.current = null;
+          e.currentTarget.style.setProperty("--x-rotation", "0deg");
+          e.currentTarget.style.setProperty("--y-rotation", "deg");
+        }}
+        className={classNames(styles.problemCard, styles[`${problem.id}`])}
+      >
         <h3>{problem.title}</h3>
         <p>{problem.statement}</p>
       </div>
