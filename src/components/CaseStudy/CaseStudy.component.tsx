064e70906ad9393bdb0f854bc6d1d935f8634a07
Wade Hammes <wade.hammes@gmail.com>
2024-06-27 15:05:58 -0400

    Feature: Add Case Studies and Cast Study page

--- src/components/CaseStudy/CaseStudy.component.tsx
+++ src/components/CaseStudy/CaseStudy.component.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
+import Image from "next/image";
 import styles from "src/components/CaseStudy/CaseStudy.module.css";
+import { Hero } from "src/components/Hero/Hero";
 import { CaseStudy } from "src/contentful/getCaseStudies";
+import { RichText } from "src/contentful/richText";
 
 interface CaseStudyTemplateProps {
   fields: CaseStudy;
@@ -9,12 +11,39 @@ interface CaseStudyTemplateProps {
 export const CaseStudyTemplate = (props: CaseStudyTemplateProps) => {
   const { fields } = props;
 
+  if (!fields) {
+    return null;
+  }
+
+  const { copy, media, pageDescription, pageTitle } = fields;
+
   return (
-    <main className={styles.caseStudy}>
-      <Link href="/case-studies">← Case Studies</Link>
-      <div>
-        <h1>{fields.title}</h1>
-      </div>
-    </main>
+    <article>
+      <Hero h1={pageTitle} subtitle={pageDescription} reducedHeight={true} />
+      <section id="case-study-copy" className="container">
+        {copy ? (
+          <div className={styles.caseStudy}>
+            <RichText document={copy} />
+          </div>
+        ) : null}
+      </section>
+      <section id="case-study-media" className="container">
+        {media.map((m) => {
+          if (m) {
+            return (
+              <Image
+                key={m?.src}
+                src={`https:${m?.src}`}
+                alt={m?.alt}
+                width={m?.width}
+                height={m?.height}
+                loading="lazy"
+                style={{ height: "auto" }}
+              />
+            );
+          }
+        })}
+      </section>
+    </article>
   );
 };
---
+++ src/contentful/types/TypeStat.ts
@@ -0,0 +1,20 @@
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from "contentful";
+
+export interface TypeStatFields {
+  entryTitle?: EntryFieldTypes.Symbol;
+  value?: EntryFieldTypes.Symbol;
+  unit?: EntryFieldTypes.Symbol;
+  caption?: EntryFieldTypes.Symbol;
+}
+
+export type TypeStatSkeleton = EntrySkeletonType<TypeStatFields, "stat">;
+export type TypeStat<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode = LocaleCode,
+> = Entry<TypeStatSkeleton, Modifiers, Locales>;
