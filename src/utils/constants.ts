f68d5e768d6629f03b298942f7df181bd9d2e30b
Wade Hammes <wadehammes@users.noreply.github.com>
2024-07-30 19:15:13 -0400

    Chore: package updates 073024 (#100)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,12 @@
 export const NAVIGATION_ID = "navigation-global";
 export const CASE_STUDIES_SLUG = "case-studies";
 export const START_YOUR_PROJECT_SLUG = "start-your-project";
+export const BLOG_SLUG = "blog";
 export const HOME_PAGE_SLUG = "home";
 export const TEST_PAGE_SLUG = "test-page";
 
 export const EXCLUDED_PAGE_SLUGS_FROM_BUILD = [
   CASE_STUDIES_SLUG,
   START_YOUR_PROJECT_SLUG,
+  BLOG_SLUG,
 ];
