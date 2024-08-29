eca9e53edb53ef35d68d4d88540f3cdb8a9363ca
Wade Hammes <wadehammes@users.noreply.github.com>
2024-08-29 16:09:06 -0400

    Chore: remove case studie redirects for prod (#106)

--- next.config.js
+++ next.config.js
@@ -109,8 +109,6 @@ module.exports = {
 const sources = [
   "/:slug(test-page.*)",
   "/case-studies/:slug(test-page.*)",
-  "/case-studies", // TODO: remove once case studies are ready
-  "/case-studies/:slug*", // TODO: remove once case studies are ready
   "/blog/:slug*", // TODO: remove once blog is ready
   "/blog", // TODO: remove once blog is ready
 ];
