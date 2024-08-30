c10d6c73c287bc5e7edd3c93a49e3c18eeb657f0
Wade Hammes <wadehammes@users.noreply.github.com>
2024-08-30 19:06:04 -0400

    Feature: update InView for quote/stats (#119)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
