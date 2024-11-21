233a88ef7ec9cd7336ede24a4c4b32a67facbdb8
Wade Hammes <wade.hammes@gmail.com>
2024-11-20 21:40:26 -0500

    Chore: package updates 112024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,23 +54,6 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <link
           rel="sitemap"
           type="application/xml"
           title="Sitemap"
