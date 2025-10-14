acf5a35acf50b3bc4b650e5a4260ff9379f7ceed
Wade Hammes <wade.hammes@gmail.com>
2025-10-14 09:49:26 -0400

    Chore: package updates 101425

--- src/hooks/mutations/useResendUnsubscribeContactApi.mutation.ts
+++ src/hooks/mutations/useResendUnsubscribeContactApi.mutation.ts
@@ -1,9 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import { api } from "src/api/urls";
+import { NewsletterFormInputs } from "src/components/NewsletterForm/NewsletterForm.component";
 
 export const useResendUnsubscribeContactApiMutation = () => {
   const mutation = useMutation({
-    mutationFn: api.resend.unsubscribe,
+    mutationFn: (variables: NewsletterFormInputs) =>
+      api.resend.unsubscribe(variables),
   });
 
   return mutation;
--- src/hooks/mutations/useSendProjectRequestEmailApi.mutation.ts
+++ src/hooks/mutations/useSendProjectRequestEmailApi.mutation.ts
@@ -1,9 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { api } from "src/api/urls";
+import { ProjectFormInputs } from "src/components/StartYourProjectForm/StartYourProjectForm.component";
 
 export const useSendProjectRequestEmailApiMutation = () => {
   const mutation = useMutation({
-    mutationFn: api.resend.projectRequest,
+    mutationFn: (
+      variables: Pick<
+        ProjectFormInputs,
+        "companyName" | "email" | "name" | "briefDescription"
+      >,
+    ) => api.resend.projectRequest(variables),
   });
 
   return mutation;
